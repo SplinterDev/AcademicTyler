2c3db16bfaf6035548b765933ff815672924814c
guichristmann <guichristmann@gmail.com>
2018-12-14 20:28:40 +0000

    Added basic code for views and buttons.

--- views.py
+++ views.py
@@ -1,21 +1,18 @@
 import pygame
 print("Shut up pygame, community! :(")
 from constants import *
+from pygame.locals import *
+from pygame.color import Color
 
-class MainView:
-    def __init__(self, width=1024, height=576, window_caption="Tyler"):
-        # comentarios em ingles ou portugues?
-        # lets use @property because yes boi
-        # https://www.programiz.com/python-programming/property
+class View:
+    def __init__(self, width, height, anchor_pos):
         self._width  = width
         self._height = height
         self._size   = (self._width, self._height)
 
-        self._screen = None
-        self._backgroundColor = BACKGROUND_COLOR
+        self._anchor_pos = anchor_pos
 
-        self._screen = pygame.display.set_mode(self._size)
-        pygame.display.set_caption(window_caption)
+        self._surface = pygame.Surface((width, height))
 
     # we dont need this but it's a nice example
     @property
@@ -37,34 +34,34 @@ class MainView:
         self._size   = (self._width, self._height)
 
     def blitSurface(self, surface, position):
-        self._screen.blit(surface, position)
+        self._surface.blit(surface, position)
 
-    def drawTiles(self):
-        # to bugando pra gerar uma surface pra cada tile
-        # e pra definir o tamanho dos tiles em relação a tela
-        # por isso vou fazer desse jeito escroto aqui soh pra ter
-        tile_size = 50
-        # horizontal lines
-        for i in range(0,self._height, tile_size):
-            pygame.draw.line(
-                self._screen,
-                pygame.Color("#FFFFFF"),
-                (0, i),
-                (self._width, i),
-                1
-            )
-        # vertical lines
-        for i in range(0,self._width, tile_size):
-            pygame.draw.line(
-                self._screen,
-                pygame.Color("#FFFFFF"),
-                (i, 0),
-                (i, self._width),
-                1
-            )
+    def getSurface(self):
+        return self._surface
+
+class MainView(View):
+    def __init__(self, width=1024, height=576, window_caption="Tyler"):
+        # Calls init of parent class, disregard anchor_pos
+        super().__init__(width, height, (0, 0))
+
+        # Creates the screen surface. Anything draw on this appears on-screen
+        # Since this is the MainView, we define the _surface as the screen surface.
+        self._surface = pygame.display.set_mode(self._size)
+
+        self._backgroundColor = BACKGROUND_COLOR
+
+        # Title for the window
+        pygame.display.set_caption(window_caption)
+
+        # TODO remove this later
+        self._hudinf = HUDView(width, 200, (0, height-200))
+
+    def drawSubViews(self):
+        self._hudinf.draw()
+        self.blitSurface(self._hudinf.getSurface(), self._hudinf._anchor_pos)
 
     def update(self):
-        self.drawTiles()
+        self.drawSubViews()
 
         pygame.display.flip()
 
@@ -74,14 +71,43 @@ class MainView:
         print("View is quitting")
         pygame.display.quit()
 
-#############################################################
-# this has to go to the controller (that doesn't exist yet) #
-#############################################################
+class HUDView(View):
+    def __init__(self, width, height, anchor_pos):
+        super().__init__(width, height, anchor_pos)
 
-if __name__ == '__main__':
-    view = View()
-    view.init()
-    run = True
-    while run:
-        update(view)
-        run = view.run
+        self._buttons = [Button(32, 32, Color('tomato1'), "hur", self.button1),
+                         Button(32, 32, Color('tomato3'), "hur", self.button1)]
+
+    def button1(self):
+        print("oi")
+
+    def draw(self):
+        self._surface.fill(Color("turquoise"))
+        
+        x_pos = 0
+        for b in self._buttons:
+            self.blitSurface(b.draw(), (x_pos, 0))
+            x_pos += 32
+
+class Button:
+    def __init__(self, width, height, color, text, callback_fn):
+        self._surface = pygame.Surface((width, height))
+
+        self._callback_fn = callback_fn
+        
+        self._color = color
+        
+        self._text = text
+
+    # Redraw the button and return its surface
+    def draw(self):
+        self._surface.fill(self._color) 
+        return self._surface
+    
+    def onClick(self):
+        self._callback_fn()
+
+    def getSurface(self):
+        return self._surface
+
+
