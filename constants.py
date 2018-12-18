f85294e32a507cb1461db72858cdb2bba9c41c38
SplinterDev <fabriciojcmontenegro@gmail.com>
2018-12-18 18:27:21 -0200

    Created constants for mouse buttons

--- constants.py
+++ constants.py
@@ -1,6 +1,12 @@
 from pygame.locals import *
+from enum import Enum
 
 WIDTH  = 1024
 HEIGHT = 576
 
 BACKGROUND_COLOR = "#0C0C0C"
+
+class Mouse(Enum):
+    BUTTON1 = 0
+    BUTTON2 = 1
+    BUTTON3 = 2
