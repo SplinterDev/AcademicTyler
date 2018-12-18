847e8a7fb87b492a0451210cd083bd2b678a30d5
SplinterDev <fabriciojcmontenegro@gmail.com>
2018-12-18 00:32:03 -0200

    Created a subview system

--- game.py
+++ game.py
@@ -27,9 +27,10 @@ class Game:
         mouse_btns = pygame.mouse.get_pressed()
         mouse_pos = pygame.mouse.get_pos()
 
-        if mouse_btns[0] == True:
-            # self.mainView.handleInput((1, mouse_pos))
-            pass
+        self.mainView.handleMouseEvents(mouse_pos, mouse_btns)
+
+        # if mouse_btns[0] == True:
+        #     self.mainView.handleInput((1, mouse_pos))
 
     def run(self):
         self.run = True
--- views_system.py
+++ views_system.py
@@ -4,21 +4,39 @@ from constants import WIDTH, HEIGHT
 
 
 
-class VisualElement:
-
-    def __init__(self, size, rel_pos=(0,0), parent=None):
-        # receives two tuples of int
+class View:
+    def __init__(self, size, rel_pos=(0,0), parent=None, color_name="black", debug_name=""):
+        # to create a view we need the size as a tuple of int
+        # rel_pos is the topleft relative to the parent
+        # (MainView doesn't have a parent)
+        # color_name is used when the default draw method is used
         self._surface = pygame.Surface(size)
         self._parent = parent
+
+        # _rel_rect stores the position relative to the parent
+        # _abs_rect stores the position relative to the window
         self._rel_rect = pygame.Rect(rel_pos, size)
         if parent:
-            abs_left = rel_pos[0] + parent.getAbsRect().left
-            abs_top  = rel_pos[1] + parent.getAbsRect().top
-
+            abs_x = rel_pos[0] + parent.getAbsRect().x
+            abs_y = rel_pos[1] + parent.getAbsRect().y
+            parent.addSubView(self)
         else:
-            abs_left = rel_pos[0]
-            abs_top  = rel_pos[1]
-        self._abs_rect = pygame.Rect((abs_left, abs_top), size)
+            abs_x = rel_pos[0]
+            abs_y = rel_pos[1]
+        self._abs_rect = pygame.Rect((abs_x, abs_y), size)
+
+        self._subviews = []
+        self._background = Color(color_name)
+        self._debug_name = debug_name
+
+    def draw(self):
+        self._surface.fill(self._background)
+        for view in self._subviews:
+            view.draw()
+            self.blit(view)
+
+    def addSubView(self, view):
+        self._subviews.append(view)
 
     def getAbsRect(self):
         return self._abs_rect
@@ -29,9 +47,9 @@ class VisualElement:
     def getRelTopLeft(self):
         return self._rel_rect.topleft
 
-    # blit receives a VisualElement
-    def blit(self, vis_elem):
-        self._surface.blit(vis_elem.getSurface(), vis_elem.getRelTopLeft())
+    # blit receives a View
+    def blit(self, view):
+        self._surface.blit(view.getSurface(), view.getRelTopLeft())
 
     def getSurface(self):
         return self._surface
@@ -39,9 +57,18 @@ class VisualElement:
     def getSize(self):
         return _rect.size
 
+    def printViewTree(self):
+        print(self)
+        if len(self._subviews) > 0:
+            for subview in self._subviews:
+                subview.printViewTree()
+
+    def __str__(self):
+        return "View {} at {}(abs)".format(self._debug_name, self._abs_rect)
 
 
-class MainView(VisualElement):
+
+class MainView(View):
     def __init__(self, size=(WIDTH, HEIGHT), window_caption="Tyler"):
         # Calls constructor of parent class
         super().__init__(size)
@@ -53,22 +80,33 @@ class MainView(VisualElement):
         # Title for the window
         pygame.display.set_caption(window_caption)
 
-        self._subviews = []
         self.createExampleSubviews()
 
     def createExampleSubviews(self):
         size = (WIDTH/2., HEIGHT/2.)
         rel_pos = (0, 0)
-        topleft = GenericView(size, rel_pos, self, "chocolate")
-        self._subviews.append(topleft)
+        topleft = View(size, rel_pos, self, "chocolate", debug_name="topleft")
 
         rel_pos = (WIDTH/2., 0)
-        topright = GenericView(size, rel_pos , self, "chocolate1")
-        self._subviews.append(topright)
+        topright = View(size, rel_pos , self, "darkolivegreen1", debug_name="topright")
 
         rel_pos = (WIDTH*0.25, HEIGHT/2.)
-        midbottom = GenericView(size, rel_pos , self, "chocolate2")
-        self._subviews.append(midbottom)
+        midbottom = View(size, rel_pos , self, "darksalmon", debug_name="midbottom")
+
+        View((100,100), (50,50), topleft, "dodgerblue1", debug_name="sub topleft")
+        View((100,100), (150,50), topright, "darkorchid1", debug_name="sub topright")
+        last_view = View((200,150), (50,50), midbottom, "deeppink1", debug_name="last_view")
+
+        Button((100,100), print_button, (25,25), last_view, "firebrick2", debug_name="button")
+
+        self.printViewTree()
+
+    def handleMouseEvents(self, mouse_pos, mouse_btns):
+        if 1 in mouse_btns:
+            print("Mouse status: {} at {}".format(mouse_btns, mouse_pos))
+
+
+
 
     def draw(self):
         for view in self._subviews:
@@ -83,13 +121,16 @@ class MainView(VisualElement):
         print("View is quitting")
         pygame.display.quit()
 
-class GenericView(VisualElement):
-    def __init__(self, size, relative_pos, parent, color_name):
-        # Calls constructor of parent class
-        super().__init__(size, relative_pos, parent)
-        self._background = Color(color_name)
+class Button(View):
+    def __init__(self, size, callback_fn, rel_pos=(0,0), parent=None, 
+                 color_name="black", debug_name=""):
+        super().__init__(size, rel_pos, parent, color_name, debug_name)
+        self._callback_fn = callback_fn
 
-    def draw(self):
-        self._surface.fill(self._background)
+    def onClick(self, *args):
+        self._callback_fn(*args)
 
+
+def print_button(btn):
+    print("Clicked {} button at {}.".format(btn, btn.getAbsRect()))
 
