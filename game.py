5036da7588bf90570ca4cccba411827b84840c84
SplinterDev <fabriciojcmontenegro@gmail.com>
2018-12-18 22:43:30 -0200

    Took away example functionalities from game.py

--- game.py
+++ game.py
@@ -9,27 +9,7 @@ class Game:
         pygame.init()
 
         self.mainView = MainView()
-        self.mainView.setClickCallback('btn', Mouse.BUTTON1, self.moveButtonLeft)
-        self.mainView.setClickCallback('btn', Mouse.BUTTON2, self.epilepsy)
-        self.mainView.setClickCallback('btn', Mouse.BUTTON3, self.moveButtonRight)
-
         self.clock = pygame.time.Clock()
-
-    # TODO this is just an example function.
-    def epilepsy(self):
-        self.mainView.subviews_dict['btn']._background = Color(sample(pygame.colordict.THECOLORS.keys(), 1)[0])
-        self.mainView.subviews_dict[0]._background = Color(sample(pygame.colordict.THECOLORS.keys(), 1)[0])
-        self.mainView.subviews_dict[1]._background = Color(sample(pygame.colordict.THECOLORS.keys(), 1)[0])
-        self.mainView.subviews_dict[2]._background = Color(sample(pygame.colordict.THECOLORS.keys(), 1)[0])
-
-    def moveButtonRight(self):
-        self.mainView.subviews_dict['btn'].move((5,0))
-
-    def moveButtonLeft(self):
-        self.mainView.subviews_dict['btn'].move((-5,0))
-
-
-
 
     def handleInput(self):
         # Updates event queue and checks for QUIT event
@@ -48,9 +28,6 @@ class Game:
         mouse_pos = pygame.mouse.get_pos()
 
         self.mainView.handleMouseEvents(mouse_pos, mouse_btns)
-
-        # if mouse_btns[0] == True:
-        #     self.mainView.handleInput((1, mouse_pos))
 
     def run(self):
         self.run = True
